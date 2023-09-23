5cdb0a6b4b5d3f85d64252b7f5ff68eda6641ff5
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-23 18:00:06 +0100

    Create Navbar.jsx

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import Desktop from "./Desktop";
+import Mobile from "./Mobile";
+
+const NavbarWrapper = styled.div`
+  /* Default styles for both desktop and mobile */
+  /* Add any common styles here */
+
+  @media (max-width: 768px) {
+    /* Media query for mobile view (adjust the width as needed) */
+    .desktop-view {
+      display: block;
+    }
+    .mobile-view {
+      display: block;
+    }
+  }
+
+  @media (min-width: 769px) {
+    /* Media query for desktop view (adjust the width as needed) */
+    .desktop-view {
+      display: ;
+    }
+    .mobile-view {
+      display: none;
+    }
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <NavbarWrapper>
+      {/* Desktop view */}
+      <div className="desktop-view">
+      <Desktop />
+      </div>
+
+      {/* Mobile view */}
+      <div className="mobile-view">
+        <Mobile />
+      </div>
+    </NavbarWrapper>
+  );
+};
+
+export default Navbar;
