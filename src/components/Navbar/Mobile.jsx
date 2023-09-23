3687f4e2d049f941b5bcf783900c4480a63ef61a
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-23 18:00:00 +0100

    Create Mobile.jsx

---
+++ src/components/Navbar/Mobile.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./navbar.css";
+
+const Mobile = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={`nav ${menuOpen ? "menu-open" : "d"}`}>
+      <div className="name">Sam</div>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`nav-list ${menuOpen ? "show-menu" : ""}`}>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Blogs</a></li>
+        {menuOpen && (
+          <li><button className="nav-button">Contact Us</button></li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Mobile;
