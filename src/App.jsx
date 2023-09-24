81eca6408314122022df863405084dbb4509889a
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-24 13:10:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 const App = () => {
   return (
   <>
   <Navbar />
+  <Hero />
   </>
   )
 }
