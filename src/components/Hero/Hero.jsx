897ed62fc5891504d6ba03cb626e5eab41dc8c43
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-24 14:38:07 +0100

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,13 @@ import "./hero.css"
 
 const Hero = () => {
     return (
-        <div>
-            <h2 className="greeting">Hello</h2>
+        <div className="hero">
+            <h2 className="greeting">Hello!</h2>
+            <h1>I'm <span className="sam">Sam,</span><br />Full Stack Developer</h1>
+            <div className="">
+            <img src="src\assets\sam.png" alt="sam-yung" className="pic" />
+            <div className="pic-bg"></div>
+            </div>
         </div>
     )
 }
