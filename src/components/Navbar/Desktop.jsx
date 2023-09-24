b5e97b6ac915ee6db970f360c3c2804e6c7379a0
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-24 12:30:10 +0100

    Update Desktop.jsx

--- src/components/Navbar/Desktop.jsx
+++ src/components/Navbar/Desktop.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import "./desktop.css"
 
 const Desktop = () => {
-  return (
-    <div className="nav">
-      <ul className="nav-list">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Blogs</a></li>
-        <li><button className="nav-button">Contact Us</button></li>
-      </ul>
-    </div>
-  );
-};
+  
+    return (
+      <div className="nav-desktop">
+        
+        <div class="image-container">
+        <img src="src\assets\s.png" alt="logo" />
+          </div>
+          <div className="naming">
+          Sam</div>
+          <ul className="nav-listing">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Blogs</a></li>
+          </ul>
+        <button className="nav-button-desk">Contact</button>
+      </div>
+    );
+  }
+  
+
 
 export default Desktop;
