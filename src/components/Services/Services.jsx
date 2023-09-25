7710bdc5dec7ef9e47e6f2b27c62a09e3b649f5c
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-25 23:57:18 +0100

    Update Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -8,8 +8,19 @@ const Services = () => {
             <h1 className="my-services">My <span className="span">Services</span></h1><br />
             <p className="lorem">Lorem Ipsum dolor sit amet, consectetur <br /> 
             adipiscing elit, sed do eiusmod tempor</p>
-        </div>
+            <div className="box"></div>
+            
         <div className="service-bar"></div>
+        <div className="cards">
+            <div className="card-1">
+            <h2 className="box-head">UI/UX Design</h2>
+                <div className="box1"></div>
+                <div className="box2"></div>
+                <div className="box3"></div>
+            </div>
+        </div>
+        </div>
+       
         </>
     )
 }
