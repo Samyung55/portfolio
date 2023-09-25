9b706f18794de69c5edc2f10a13d992e476bee73
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-25 23:25:23 +0100

    Create Services.jsx

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./services.css"
+
+const Services = () => {
+    return (
+        <>
+        <div className="service-bg">
+            <h1 className="my-services">My <span className="span">Services</span></h1><br />
+            <p className="lorem">Lorem Ipsum dolor sit amet, consectetur <br /> 
+            adipiscing elit, sed do eiusmod tempor</p>
+        </div>
+        <div className="service-bar"></div>
+        </>
+    )
+}
+
+export default Services
