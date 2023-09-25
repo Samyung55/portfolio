f61fd923969afa4c8395f60a341e38f1b8bca87d
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-25 23:24:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Services from './components/Services/Services'
 
 const App = () => {
   return (
   <>
   <Navbar />
   <Hero />
+  <Services />
   </>
   )
 }
