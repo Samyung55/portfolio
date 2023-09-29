58985cf7c43f02a53e3ff1c9a2097412071a58fc
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-30 00:35:44 +0530

    Update Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
         <div className="service-bg">
             <h1 className="my-services">My <span className="span">Services</span></h1><br />
             <p className="lorem">I'm thrilled to showcase the range of services   I offer to meet <br />your unique needs.
-            With passion I bring creativity, innovation, and  <br /> expertise to every project. Here's how I can help you</p>
+            With passion I bring creativity, innovation, <br />and   expertise to every project. Here's how I can help you</p>
             <div className="box"></div>
             
         <div className="service-bar"></div>
@@ -23,7 +23,11 @@ const Services = () => {
             
             </div>
             
-            <div className="circle"><div className="inner-arrow"><img src="src\assets\arrow-removebg-preview.png" /></div></div>
+            <div className="circle">
+                <div className="inner-arrow">
+                    <img src="src\assets\arrow-removebg-preview.png" />
+                    </div>
+                </div>
             
             <div className="card-2">
             <h2 className="box-head">Product Design</h2>
@@ -35,7 +39,11 @@ const Services = () => {
                 </div>
             
             </div>
-            <div className="circle-2"><div className="inner-arrow2"><img src="src\assets\arrow-removebg-preview.png" /></div></div>
+            <div className="circle">
+                <div className="inner-arrow">
+                    <img src="src\assets\arrow-removebg-preview.png" />
+                    </div>
+                </div>
 
 
             <div className="card-3">
@@ -48,7 +56,11 @@ const Services = () => {
                 </div>
             
             </div>
-            <div className="circle-3"><div className="inner-arrow3"><img src="src\assets\arrow-removebg-preview.png" /></div></div>
+            <div className="circle">
+                <div className="inner-arrow">
+                    <img src="src\assets\arrow-removebg-preview.png" />
+                    </div>
+                </div>
             
         </div>
             
