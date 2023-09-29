711274cd301b26326b25e604d531b02bbe9eb636
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-30 00:35:49 +0530

    Create Work.jsx

---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./work.css"
+
+const Work = () => {
+    return (
+        <div className="work">
+            <h2 className="experience">My Work Experience</h2>
+            <div className="companies">
+                 <div className="work-1">
+                    <div className="company-1">
+                    <h2 className="title-1">
+                        Self-Employed, Lagos
+                    </h2>
+                    <p className="years">Sep 2020 - July 2023</p>
+                    </div>
+                    <div className="intersection1">
+                    <div className="intersection2"></div></div>
+                    <div className="position-1">
+                    <h2 className="title-2">
+                       Visual Designer
+                    </h2>
+                    <p className="years">Lorem ipsum dolor sit amet consectetur 
+                    <br />Doloribus suscipit, odio sed tenetur molestiae neque recusandae adipisci.
+                    </p>
+                    </div>
+                </div>
+                <div className="inter-bar"></div>
+                <div className="work-1">
+                    <div className="company-1">
+                    <h2 className="title-1">
+                    Insightancer
+                    </h2>
+                    <p className="years">Aug 2016 - July 2020</p>
+                    </div>
+                    <div className="intersection1">
+                    <div className="intersection4"></div></div>
+                    <div className="position-1">
+                    <h2 className="title-2">
+                    UI/UX Designer
+                    </h2>
+                    <p className="years">Lorem ipsum dolor sit amet consectetur 
+                    <br />Doloribus suscipit, odio sed tenetur molestiae neque recusandae adipisci.
+                    </p>
+                    </div>
+                </div>
+                
+                <div className="inter-bar"></div>
+                <div className="work-1">
+                    <div className="company-1">
+                    <h2 className="title-1">
+                    KG Design Studio
+                    </h2>
+                    <p className="years">Aug 2016 - July 2020</p>
+                    </div>
+                    <div className="intersection1">
+                    <div className="intersection2"></div></div>
+                    <div className="position-1">
+                    <h2 className="title-2">
+                    Web Designer
+                    </h2>
+                    <p className="years">Lorem ipsum dolor sit amet consectetur 
+                    <br />Doloribus suscipit, odio sed tenetur molestiae neque recusandae adipisci.
+                    </p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Work
