937389eb65e3f44cb4d8501a750edb8d5ccb9648
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-30 21:45:51 +0100

    Update Carousel.jsx

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,58 +1,52 @@
 import React, { useState } from "react";
-import "./carousel.css"
+import "./carousel.css";
 
-const Carousel = ({ project }) => {
-    const [currentIndex, setCurrentIndex] = useState(0)
+const Carousel = ({ projects }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === projects.length - 1 ? 0 : prevIndex + 1));
+  }
 
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === project.length - 1 ? 0 : prevIndex + 1));
-    }
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1))
+  }
 
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? project.length - 1 : prevIndex - 1))
-    }
+  const currentProject = projects[currentIndex];
 
-    const nextIndex = (currentIndex + 1) % project.length;
-
-
-    return (
-        <div className="carousel">
-            <div className="heading">
-                <h2 className="look-at">Let's Have a Look at my <br /><span className="folio">Portfolio</span></h2>
-                <button className="see-more"><p>See More</p> <img src="src\assets\arrow-up-right-svgrepo-com.svg" /></button>
-            </div>
-            <div className="carousel-box">
-            <button className="carousel-btn" onClick={prevSlide}><img src="src\assets\arrow-sm-left-svgrepo-com.svg" /></button> 
-            <div className="carousel-container">
-                <div className="slide">
-                    <img src={project.images[currentIndex]} alt={`Slide ${currentIndex}`} />
-                </div>
-                <div className="slide">
-                    <img src={project.images[nextIndex]} alt={`Slide ${nextIndex}`} />
-                </div>
-            </div>
-            <button className="carousel-btn-2" onClick={nextSlide}><img src="src\assets\arrow-sm-right-svgrepo-com.svg" /></button>
-            </div>
-            <div className="project-info">
-            <div className="tags">
-                <h3>UI/UX Design</h3>
-                <h3>Landing Page</h3>
-                <h3>Wireframe</h3>
-            </div>
-            <div className="project-title">
-                Food Express - Food Delivery Solution
-            </div>
-            <p className="project-description">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Consequuntur quis officia modi expedita soluta repellat, 
-                possimus provident eum animi 
-                necessitatibus nostrum amet non vitae, molestias velit eligendi ab ipsum sint.
-            </p>
-            </div>
+  return (
+    <div className="carousel">
+      <div className="heading">
+        <h2 className="look-at">Let's Have a Look at my <br /><span className="folio">Portfolio</span></h2>
+        <button className="see-more"><p>See More</p> <img src="src\assets\arrow-up-right-svgrepo-com.svg" alt="See More" /></button>
+      </div>
+      <div className="carousel-box">
+        <button className="carousel-btn" onClick={prevSlide}><img src="src\assets\arrow-sm-left-svgrepo-com.svg" alt="Previous" /></button>
+        <div className="carousel-container">
+          <div className="slide">
+            <img src={currentProject.images[0]} alt={`Slide ${currentIndex}`} />
+          </div>
+          <div className="slide">
+            <img src={currentProject.images[1]} alt={`Slide ${currentIndex + 1}`} />
+          </div>
         </div>
-    )
+        <button className="carousel-btn-2" onClick={nextSlide}><img src="src\assets\arrow-sm-right-svgrepo-com.svg" alt="Next" /></button>
+      </div>
+      <div className="project-info">
+        <div className="tags">
+          <h3>{currentProject.tags[0]}</h3>
+          <h3>{currentProject.tags[1]}</h3>
+          <h3>{currentProject.tags[2]}</h3>
+        </div>
+        <div className="project-title">
+          {currentProject.name}
+        </div>
+        <p className="project-description">
+          {currentProject.description}
+        </p>
+      </div>
+    </div>
+  )
 }
 
-export default Carousel
+export default Carousel;
