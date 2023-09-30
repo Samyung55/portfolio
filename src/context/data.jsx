70321bbd65d32a1acf34d8b931864ddc4309d3e9
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-30 19:39:10 +0100

    Create data.jsx

---
+++ src/context/data.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+import phone from "./assets/phone.png"
+import development from './assets/development.png'
+
+export const ProjectData = [
+    {
+        id: 1,
+        tags: ['UI/UX Design', 'Landing Page', 'WireFrame'],
+        name: 'Food Express - Food Delivery Solution',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aut reprehenderit distinctio quaerat quae mollitia odio minima labore nostrum recusandae magni assumenda fugiat ullam commodi magnam atque, doloremque molestiae dolores?',
+        images: [phone, development]
+    },
+    {
+        id: 2,
+        tags: ['UI/UX Design', 'Web Application', 'Front-End'],
+        name: 'Estating - Find Your Dream Home',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aut reprehenderit distinctio quaerat quae mollitia odio minima labore nostrum recusandae magni assumenda fugiat ullam commodi magnam atque, doloremque molestiae dolores?',
+        images: [phone, development]
+    }
+]
+
