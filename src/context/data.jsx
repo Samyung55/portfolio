e7f8e1038b79ac19ebe6fa994c3a7d18e4f407e9
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-02 18:47:04 +0100

    Update data.jsx

--- src/context/data.jsx
+++ src/context/data.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import phone from "../assets/phone.png";
 import development from '../assets/development.png';
+import est from '../assets/projects/est.png'
+import mobile1 from '../assets/projects/mobile1.png'
+import mobile from '../assets/projects/mobile.png'
 
 export const ProjectData = [
     {
@@ -15,6 +18,13 @@ export const ProjectData = [
         tags: ['UI/UX Design', 'Web Application', 'Front-End'],
         name: 'Estating - Find Your Dream Home',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aut reprehenderit distinctio quaerat quae mollitia odio minima labore nostrum recusandae magni assumenda fugiat ullam commodi magnam atque, doloremque molestiae dolores?',
-        images: [phone, development]
+        images: [est, development]
+    },
+    {
+        id: 3,
+        tags: ['React-Native', 'Mobile Application', 'Full-Stack'],
+        name: 'Crypto-Exchange Mobile App',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aut reprehenderit distinctio quaerat quae mollitia odio minima labore nostrum recusandae magni assumenda fugiat ullam commodi magnam atque, doloremque molestiae dolores?',
+        images: [mobile, mobile1]
     }
 ];
