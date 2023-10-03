7ec650d2bd9c8db728404458bfdd5b66a9e2b262
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-03 19:53:47 +0100

    Update Testimonial.jsx

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./testimonial.css"
 
-const Testimonial = () => {
+const Testimonial = ({ clients }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isMobile, setIsMobile] = useState(false);
+  
+    const nextSlide = () => {
+      setCurrentIndex((prevIndex) => (prevIndex === clients.length - 1 ? 0 : prevIndex + 1));
+    };
+  
+    const prevSlide = () => {
+      setCurrentIndex((prevIndex) => (prevIndex === 0 ? clients.length - 1 : prevIndex - 1));
+    };
+  
+    const currentClient = clients[currentIndex];
+
+    useEffect(() => {
+      // Check if the screen width is less than or equal to 767px (typical mobile width)
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 767);
+      };
+  
+      // Listen for window resize events
+      window.addEventListener("resize", handleResize);
+  
+      // Initial check on component mount
+      handleResize();
+  
+      // Cleanup the event listener
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+
     return (
     <div className="testimonials">
         <div className="test-head">
@@ -16,38 +47,31 @@ const Testimonial = () => {
         </div>
         <div className="test-array">
             <div className="stars">
-                <img src="src\assets\icons8-star-64.png"></img>
-                <img src="src\assets\icons8-star-64.png"></img>
-                <img src="src\assets\icons8-star-64.png"></img>
-                <img src="src\assets\icons8-star-64.png"></img>
-                <img src="src\assets\icons8-star-64.png"></img>
-                <h3>5.0</h3>
+            {currentClient.starimg}
+                <h3>{currentClient.stars}</h3>
             </div>
             <div className="test-para">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Reprehenderit unde veritatis libero dolorem provident iure distinctio, 
-                excepturi ipsa hic aut quidem minima ut at. 
-                Nesciunt dolor officia iste ea vero.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Reprehenderit unde veritatis libero dolorem provident iure distinctio, 
-                excepturi ipsa hic aut quidem minima ut at. 
-                Nesciunt dolor officia iste ea vero.
+                {currentClient.comment}
             </div>
             <div className="client-info">
                 <div className="client-img">
                     <img src="src\assets\clients\henry.png"></img>
                 </div>
                 <div className="client-name">
-                    <h3>Henry, Arthur</h3>
-                    <p>CEO, Food Express</p>
+                    <h3>{currentClient.name}</h3>
+                    <p>{currentClient.company}</p>
                 </div>
                 <div className="quote">
                     <img src="src\assets\icons8-quote-32.png"></img>
                 </div>
             </div>
         </div>
+        <div className="test-buttons">
+        <button className="test-btn" onClick={prevSlide}><img src="src\assets\arrow-sm-left-svgrepo-com.svg" alt="Previous" /></button>
+        <button className="test-btn-2" onClick={nextSlide}><img src="src\assets\arrow-sm-right-svgrepo-com.svg" alt="Next" /></button>
+        </div>
     </div>
     )
 }
 
-export default Testimonial
+export default Testimonial 
