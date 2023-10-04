483374c9b691ef1ecab4437164790f93d4cb2564
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-04 11:55:36 +0100

    Update Newsletter.jsx

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -19,16 +19,27 @@ const Newsletter = () => {
             </div>
             <div className="achievements">
                 <div className="achieve-1">
-                    <img src="" alt="" />
+                    <img src="src\assets\icons8-approved-32.png" alt="" />
                     <p>4.9/5 Average Ratings</p>
                 </div>
                 <div className="achieve-1">
-                    <img src="" alt="" />
+                    <img src="src\assets\icons8-approved-32.png" alt="" />
                     <p>25+ Winning Awards</p>
                 </div>
                 <div className="achieve-1">
-                    <img src="" alt="" />
+                    <img src="src\assets\icons8-approved-32.png" alt="" />
                     <p>Certified Designer & Developer</p>
+                </div>
+            </div>
+            <div className="work-lists">
+                <div>
+                    <ul>
+                        <li><h3>Design</h3></li>
+                        <li><h3>App Design</h3></li>
+                        <li><h3>Backend</h3></li>
+                        <li><h3>Frontend</h3></li>
+                        <li><h3>Wireframe</h3></li>
+                    </ul>
                 </div>
             </div>
         </div>
