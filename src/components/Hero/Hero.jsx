07802d48b125eff1d6c7b6a173f6d83307974279
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-07 10:42:26 +0100

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,47 +1,144 @@
-import React from "react";
-import "./hero.css"
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./hero.css";
 
 const Hero = () => {
-    return (
-        <>
-        <div className="hero">
-            <h2 className="greeting">Hello!</h2>
-            <h1>I'm <span className="sam">Sam,</span><br />Full Stack Developer</h1>
-            <div className="">
-            <img src="src\assets\sam.png" alt="sam-yung" className="pic" />
-            <div className="pic-bg"></div>
-            </div>
-            <div className="link-box">
-            <button className="portfolio">Portfilio </button>
-            <button className="hire-me">Hire Me</button>
-            </div>
-        </div>
-         <div className="hero-texts">
-         <h2 className="quote">
-            <img src="src\assets\quote-left-svgrepo-com.svg" />
-         </h2>
-         <p className="para">Sam's exceptional product design <br /> ensured our website success.<br />
-             Highly recommended!
-         </p>
-         <h2 className="serve">450+</h2>
-         <p className="para-2">Client Served
-         </p>
-     </div>
-     <div className="hero-texts-2">
-         
-            <img src="src\assets\icons8-star-64.png" />
-            <img src="src\assets\icons8-star-64.png" />
-            <img src="src\assets\icons8-star-64.png" />
-            <img src="src\assets\icons8-star-64.png" />
-            <img src="src\assets\icons8-star-64.png" />
-         <h2 className="serve-2">5 Years</h2>
-         <p className="para-3">Experts
-         </p>
-         <div className="bar"></div>
-     </div>
-    
-     </>
-    )
-}
+  const jobTitles = [
+    'Full Stack Developer',
+    'Senior Visual Designer',
+    'Product Designer'
+    // Add more job titles here
+  ];
+  const [currentIndex, setCurrentIndex] = useState(1);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % jobTitles.length);
+    }, 5000); // Change the duration as needed
 
-export default Hero
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div className="hero">
+      <motion.h2
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="greeting"
+        >
+          Hello!
+        </motion.h2>
+        <h1>
+          I'm <span className="sam">Sam,</span> <br />
+          <AnimatePresence mode="wait">
+            <motion.span
+              key={jobTitles[currentIndex]}
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -20, transition: { duration: 1.5 } }}
+              transition={{ type: 'spring', damping: 10, stiffness: 70 }}
+            >
+              {jobTitles[currentIndex]}
+            </motion.span>
+          </AnimatePresence>
+        </h1>
+
+        <div className="">
+          <img src="src\assets\sam.png" alt="sam-yung" className="pic" />
+          <div className="pic-bg"></div>
+        </div>
+        <div className="link-box">
+          <button className="portfolio">Portfilio </button>
+          <button className="hire-me">Hire Me</button>
+        </div>
+      </div>
+      <div className="hero-texts">
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="quote"
+        >
+          <img src="src\assets\quote-left-svgrepo-com.svg" />
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="para"
+        >
+          Sam's exceptional product design <br /> ensured our website success. <br />
+          Highly recommended!
+        </motion.p>
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="serve"
+        >
+          450+
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="para-2"
+        >
+          Client Served
+        </motion.p>
+      </div>
+      <div className="hero-texts-2">
+        <motion.img
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          src="src\assets\icons8-star-64.png"
+        />
+        <motion.img
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          src="src\assets\icons8-star-64.png"
+        />
+        <motion.img
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          src="src\assets\icons8-star-64.png"
+        />
+        <motion.img
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          src="src\assets\icons8-star-64.png"
+        />
+        <motion.img
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          src="src\assets\icons8-star-64.png"
+        />
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="serve-2"
+        >
+          5 Years
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="para-3"
+        >
+          Experts
+        </motion.p>
+        <div className="bar"></div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
