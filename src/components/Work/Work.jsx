f5f1cc9d271ccd1316657db0920eaa519a54e318
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-11 10:37:36 +0100

    Update Work.jsx

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -19,12 +19,16 @@ const Work = () => {
                     <h2 className="title-2">
                        MERN Stack Developer
                     </h2>
-                    <p className="years">Lorem ipsum dolor sit amet consectetur 
-                    <br />Doloribus suscipit, odio sed tenetur molestiae neque recusandae adipisci.
+                    <p className="years">As a self-employed MERN Stack Developer, 
+                    I worked on a variety of web development projects. My responsibilities 
+                    included designing and building web applications using the MERN 
+                    (MongoDB, Express.js, React, and Node.js) stack. I developed responsive user interfaces, 
+                    implemented server-side logic, and managed databases. I also collaborated with clients to understand their requirements, 
+                    ensuring the delivery of high-quality, custom web solutions.
                     </p>
                     </div>
                 </div>
-                <div className="inter-bar"></div>
+                <div className="inter-bar-1"></div>
                 <div className="work-1">
                     <div className="company-1">
                     <h2 className="title-1">
@@ -38,13 +42,14 @@ const Work = () => {
                     <h2 className="title-2">
                     Developer / Director of ICT
                     </h2>
-                    <p className="years">Lorem ipsum dolor sit amet consectetur 
-                    <br />Doloribus suscipit, odio sed tenetur molestiae neque recusandae adipisci.
+                    <p className="years">During my tenure at Juplam Group, 
+                    I held the dual role of Developer and Director of Information and Communication Technology (ICT). I managed and directed the ICT department, overseeing the organization's technology infrastructure and strategies. As a developer, I led a team in creating innovative software solutions and web applications. This involved designing and implementing efficient code, collaborating with cross-functional teams, a
+                    nd ensuring the successful delivery of projects that met business objectives.
                     </p>
                     </div>
                 </div>
                 
-                <div className="inter-bar"></div>
+                <div className="inter-bar-2"></div>
                 <div className="work-1">
                     <div className="company-1">
                     <h2 className="title-1">
@@ -58,8 +63,10 @@ const Work = () => {
                     <h2 className="title-2">
                     Web/Product Designer
                     </h2>
-                    <p className="years">Lorem ipsum dolor sit amet consectetur 
-                    <br />Doloribus suscipit, odio sed tenetur molestiae neque recusandae adipisci.
+                    <p className="years">As a Web/Product Designer at Dherbalbody, 
+                    I played a key role in the design and development of the company's online presence. 
+                    I created visually appealing and user-friendly websites and digital products. I used my expertise in UI/UX design to enhance the overall user experience and engagement. Additionally, I collaborated with the marketing team to ensure brand consistency and worked on various design projects to 
+                    promote the company's products and services, contributing to the company's online success.
                     </p>
                     </div>
                 </div>
