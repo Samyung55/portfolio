01a0adcd3375bc9744c6f0d7492f00b15d61ba4c
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-17 08:23:36 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,11 @@
 import React, {useState} from "react";
+import s from "../../assets/s.png"
+import arrowUp from "../../assets/arrow-up-right-svgrepo-com.svg"
+import twitter from "../../assets/socials/icons8-twitter.png"
+import instagram from "../../assets/socials/icons8-instagram.png"
+import behance from "../../assets/socials/icons8-behance-32.png"
+import github from "../../assets/socials/icons8-github-64.png" 
+import sendimg from "../../assets/socials/icons8-send-48.png" 
 import "./footer.css"
 
 const Footer = () => {
@@ -29,35 +36,38 @@ const Footer = () => {
         alert("Error sending notification email.");
       });
   };
+  function openInstagram() {
+    window.location.href = 'https://www.instagram.com/yungofficial5';
+  }
 
     return (
         <div className="footer">
             <div className="ft-heading">
                 <h1>Let's Connect there</h1>
-                <button className="ft-hire-me"><p>Hire Me</p> <img src="src\assets\arrow-up-right-svgrepo-com.svg" alt="See More" /></button>
+                <button onClick={openInstagram} className="ft-hire-me"><p>Hire Me</p> <img src={arrowUp} alt="See More" /></button>
                 
             </div>
             <div className="footer-bar"></div>
         <div className="blocks">
             <div className="block-1">
                 <div className="my-sam">
-                    <img src="src\assets\s.png" />
+                    <img src={s} />
                     <h2>Sam</h2>
                 </div>
                 <p>I'm dedicated to delivering excellence in every project. Your satisfaction is my priority. Don't hesitate to reach out and experience top-notch service. Let's turn your ideas into reality!
                 </p>
                 <div className="social-btn">
                     <div className="icon-bg">
-                        <a href="https://www.behance.net/yungmdl"><img src="src\assets\socials\icons8-behance-32.png" alt="behance"/></a>
+                        <a href="https://www.behance.net/yungmdl"><img src={behance} alt="behance"/></a>
                     </div>
                     <div className="icon-bg">
-                        <a href="https://github.com/Samyung55"><img src="src\assets\socials\icons8-github-64.png" alt="github"/></a>
+                        <a href="https://github.com/Samyung55"><img src={github} alt="github"/></a>
                     </div>
                     <div className="icon-bg">
-                        <a href="https://www.instagram.com/yungofficial5/"><img src="src\assets\socials\icons8-instagram-50 (2).png" alt="instagram"/></a>
+                        <a href="https://www.instagram.com/yungofficial5/"><img src={instagram} alt="instagram"/></a>
                     </div>
                     <div className="icon-bg">
-                        <a href="#"><img src="src\assets\socials\icons8-twitter-50 (1).png" alt="twitter"/></a>
+                        <a href="#"><img src={twitter} alt="twitter"/></a>
                     </div>
                 </div>
             </div>
@@ -77,7 +87,7 @@ const Footer = () => {
             
             <div className="block-3">
                 <h3>Contact</h3>
-                <p>+1-343-556-5996</p>
+                <p>+1-731-474-1192 (Whatsapp)</p>
                 <p>samyung05@gmail.com</p>
             </div>
             </div>
@@ -91,7 +101,7 @@ const Footer = () => {
                     onChange={handleEmailChange}
                     />
                     <button onClick={handleSubscribe}>
-                    <img src="src/assets/socials/icons8-send-48.png" alt="icon-btn" />
+                    <img src={sendimg} alt="icon-btn" />
                     </button>
                 </div>
         </div>
