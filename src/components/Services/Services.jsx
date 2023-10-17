c2eedf5fb968b5763397f5db006d43e8edeb215e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-17 08:23:50 +0100

    Update Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./services.css"
+import phone from "../../assets/phone.png"
+import foodimg from "../../assets/projects/food.png"
+import fruitimg from "../../assets/projects/fruit-1.png"
+import circlearrow from "../../assets/arrow-removebg-preview.png"
 
 const Services = () => {
     return (
         <>
-        <div className="service-bg">
+        <div id="services" className="service-bg">
             <h1 className="my-services">My <span className="span">Services</span></h1><br />
             <p className="lorem">I'm thrilled to showcase the range of services   I offer to meet <br />your unique needs.
             With passion I bring creativity, innovation, <br />and   expertise to every project. Here's how I can help you</p>
             <div className="box"></div>
             
         <div className="service-bar"></div>
+
         <div className="cards">
             <div className="card-1">
             <h2 className="box-head">UI/UX Design</h2>
@@ -18,50 +23,52 @@ const Services = () => {
 
                 <div className="box1"></div>
                 <div className="box2"></div>
-                <div className="box3"><img src="src\assets\phone.png" className="phone"/>
+                <div className="box3"><img src={phone} className="phone"/>
                 </div>
             
             </div>
-            
+            <a href="https://www.behance.net/yungmdl">
             <div className="circle">
                 <div className="inner-arrow">
-                    <img src="src\assets\arrow-removebg-preview.png" />
+                    <img src={circlearrow} />
                     </div>
                 </div>
-            
+            </a>
             <div className="card-2">
             <h2 className="box-head">Product Design</h2>
             <div className="card-bar"></div>
 
                 <div className="box1"></div>
                 <div className="box2"></div>
-                <div className="box3"><img src="src\assets\projects\food.png" className="phone"/>
+                <div className="box3"><img src={foodimg} className="phone"/>
                 </div>
             
             </div>
+            <a href="https://www.behance.net/yungmdl">
             <div className="circle-2">
                 <div className="inner-arrow">
-                    <img src="src\assets\arrow-removebg-preview.png" />
+                    <img src={circlearrow} />
                     </div>
                 </div>
-
-
+                </a>
+                
             <div className="card-3">
             <h2 className="box-head">App Development</h2>
             <div className="card-bar"></div>
 
                 <div className="box1"></div>
                 <div className="box2"></div>
-                <div className="box3"><img src="src\assets\projects\fruit-1.png" className="phone"/>
+                <div className="box3"><img src={fruitimg} className="phone"/>
                 </div>
             
             </div>
+            <a href="https://github.com/Samyung55">
             <div className="circle-3">
                 <div className="inner-arrow">
-                    <img src="src\assets\arrow-removebg-preview.png" />
+                    <img src={circlearrow} />
                     </div>
                 </div>
-            
+            </a>
         </div>
             
             
