47c8fa2fcb48ff276bad98f66a94197b6b424150
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-17 08:23:24 +0100

    Update Desktop.jsx

--- src/components/Navbar/Desktop.jsx
+++ src/components/Navbar/Desktop.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import "./desktop.css"
+import s from "../../assets/s.png"
 
 const Desktop = () => {
   
+  function openInstagram() {
+    window.location.href = 'https://www.instagram.com/yungofficial5';
+  }
+
     return (
       <div className="nav-desktop">
           <div className="naming">
-          <img src="src\assets\s.png" alt="logo" />
+          <img src={s} alt="logo" />
           <h3>Sam</h3>
           </div>
           <ul className="nav-listing">
             <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Blogs</a></li>
+            <li><a href="#work">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#testimonials">Testimonials</a></li>
           </ul>
-        <button className="nav-button-desk">Contact</button>
+        <button onClick={openInstagram} className="nav-button-desk">Resume</button>
       </div>
     );
   }
